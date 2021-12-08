b8655cb9ac3cf2fd2502a305a9ff7861b5118edf
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-08 16:45:40 -0500

    set up fetch in action files

---
+++ src/actions/postsAction.js
@@ -0,0 +1,19 @@
+export const fetchPosts = () => {
+    return (dispatch) => {
+        fetch('http://127.0.0.1:3000/posts')
+        .then(resp => resp.json())
+        .then(posts => dispatch({ type: 'FETCH_POSTS', payload: posts}))
+    }
+}
+
+export const addPost = (post) => {
+    return dispatch => {
+        fetch('http://127.0.0.1:3000/posts', {
+            method: 'POST',
+            body: JSON.stringify(post),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(resp => resp.json())
+        .then(post => dispatch({ type: 'ADD_POST', payload: post}))
+    }
+}
