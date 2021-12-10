32cbc30518a42d1aaef781610cf6b37c42906702
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-10 06:39:14 -0500

    set up index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+import {ReactDom} from "react-dom"
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+import App from './components/App'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
